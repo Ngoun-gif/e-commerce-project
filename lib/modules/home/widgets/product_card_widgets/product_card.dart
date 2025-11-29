76e4079ed183e7c0df6784f6ac24fb34b62620b8
Ngoun-gif <khemhengnguon@gmail.com>
2025-11-29 18:53:55 +0700

    fixing product card UI improvement

---
+++ lib/modules/home/widgets/product_card_widgets/product_card.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecom/modules/home/models/product.dart';
+import 'package:flutter_ecom/modules/home/widgets/product_card_widgets/product_card_details_section.dart';
+import 'package:flutter_ecom/modules/home/widgets/product_card_widgets/product_card_image_section.dart';
+
+import '../../../routers/app_routes.dart';
+
+
+
+class ProductCard extends StatelessWidget {
+  final ProductModel product;
+
+  const ProductCard({super.key, required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      borderRadius: BorderRadius.circular(16),
+      onTap: () {
+        Navigator.pushNamed(
+          context,
+          AppRoutes.productDetail,
+          arguments: product.id,
+        );
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(16),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 12,
+              offset: const Offset(0, 4),
+              spreadRadius: 1,
+            ),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Image Section
+            ProductCardImageSection(product: product),
+
+            // Product Details Section
+            Expanded(
+              child: ProductCardDetailsSection(product: product),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
